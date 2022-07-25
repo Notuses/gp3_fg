88ca6c181e029838e4fdda55e4336c414b9cea91
Notuses <56674121+Notuses@users.noreply.github.com>
2022-07-25 15:53:41 +0200

    Add files via upload

---
+++ GP3_Team03/Public/Player/GP3PlayerCameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "GP3PlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP3_TEAM03_API AGP3PlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};
